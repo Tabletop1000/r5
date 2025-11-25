98b1cd3c61ecd0b1c3fda84a1ec702c821c20aa1
Jadon Dutra <jadondutra@gmail.com>
2025-11-25 21:56:00 +1100

    allow passing device address to motornode console

--- src/r5_description/hardware/motornode_console.cpp
+++ src/r5_description/hardware/motornode_console.cpp
@@ -1,12 +1,23 @@
 #include "serial_device.hpp"
 #include <iostream>
+#include <thread>
 
 using namespace std;
 
-int main()
+int main(int argc, char* argv[])
 {
+    if(argc < 2)
+    {
+        std::cout << "Usage: " << argv[0] << " <com port>" << std::endl;
+        return EXIT_FAILURE;
+    } else {
+
+        std::cout << "Connecting to " << argv[1] << std::endl;
+        std::this_thread::sleep_for(1s);
+    }
+
     SerialDevice motor;
-    motor.init("/dev/ttyACM0",B115200);
+    motor.init(argv[1],B115200);
     float v1,v2,p1,p2;
     while(1)
     {
